bff0dd986357baaeae549ad567736fce2411fadb
ayeshag7 <ayeshaiqbal2592@gmail.com>
2024-08-28 10:34:09 +0500

    Added small screen adaptability to a component

--- src/components/VideoComponent.js
+++ src/components/VideoComponent.js
@@ -9,11 +9,11 @@ const VideoComponent = () => {
   ];
 
   return (
-    <div className="flex justify-center gap-x-8 my-16">
+    <div className="flex flex-col md:flex-row justify-center gap-x-8 gap-y-8 my-16">
       {videos.map((video, index) => (
         <div
           key={index}
-          className="w-[576px] max-w-2xl rounded-lg overflow-hidden shadow-custom transform hover:scale-105 transition-transform duration-300"
+          className="w-72 md:w-[576px] max-w-lg md:max-w-2xl rounded-lg overflow-hidden shadow-custom transform hover:scale-105 transition-transform duration-300"
         >
           <video width="100%" height="auto" controls className="rounded-lg">
             <source src={video.src} type={video.type} />

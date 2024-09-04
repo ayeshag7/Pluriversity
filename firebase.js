18da79bbaaa3d395e0a019187525e3821b237f1f
ayeshag7 <ayeshaiqbal2592@gmail.com>
2024-09-04 11:10:34 +0500

    Added Firebase Config file

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARSGjdFXGefxk4OwS0-hjo-cTvJe3Owso",
+  authDomain: "pluriversity-7b3be.firebaseapp.com",
+  projectId: "pluriversity-7b3be",
+  storageBucket: "pluriversity-7b3be.appspot.com",
+  messagingSenderId: "603627453987",
+  appId: "1:603627453987:web:68fed0a6c470a1187ffb96",
+  measurementId: "G-9RBLRV0FLV"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
